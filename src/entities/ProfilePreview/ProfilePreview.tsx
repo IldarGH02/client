710e4a4806bcfd123ad3b50a625f456bbca2adb8
Ильдар Халилов <ildarhalilov@MacBook-Pro-Ildar.local>
2024-09-21 21:48:53 +0400

    fix avatar

--- src/entities/ProfilePreview/ProfilePreview.tsx
+++ src/entities/ProfilePreview/ProfilePreview.tsx
@@ -4,11 +4,12 @@ import { Context } from "../.."
 
 export const ProfilePreview = observer(() => {
    const { userStore } = useContext(Context)
+   const avatar = userStore.user?.photo_url
 
     return (
         <div className="profile__preview">
             <div className="profile__avatar">
-                <img src={userStore.user?.photo_url}/>
+                <img src={avatar}/>
             </div>
             <div className="profile__info">
                 <h3 className="profile__info-text">
