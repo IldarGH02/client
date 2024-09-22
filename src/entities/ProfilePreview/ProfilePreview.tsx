592e03796f732102524e0a6ce6f5e42078b18e9f
Ильдар Халилов <ildarhalilov@MacBook-Pro-Ildar.local>
2024-09-22 21:01:21 +0400

    fix avatar

--- src/entities/ProfilePreview/ProfilePreview.tsx
+++ src/entities/ProfilePreview/ProfilePreview.tsx
@@ -1,21 +1,31 @@
 import { observer } from "mobx-react-lite";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../..";
 
 import "./ProfilePreview.scss";
+import image from "../../shared/assets/image/708382_people_512x512.png"
 
 export const ProfilePreview = observer(() => {
-   const { userStore } = useContext(Context)
-   const avatar = userStore.user?.photo_url
+    const [img, setImg] = useState<string>('')
+
+    const { userStore } = useContext(Context)
+    const avatar = userStore.user?.photo_url
+
+    if(avatar){
+        setImg(avatar)
+    } else {
+        setImg(image)
+    }
 
     return (
         <div className="profile__preview">
             <div className="profile__avatar">
-                <img src={`${avatar}`}/>
+                <img src={avatar}/>
             </div>
             <div className="profile__info">
                 <h3 className="profile__info-text">
-                    {userStore.user?.first_name}
+                    {/* {userStore.user?.first_name} */}
+                    101lab
                 </h3>
             </div>
         </div>
