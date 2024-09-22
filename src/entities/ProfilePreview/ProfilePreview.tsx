08dbf05dad6d03bf13c1d6dbec5442731346683f
Ильдар Халилов <ildarhalilov@MacBook-Pro-Ildar.local>
2024-09-22 21:07:58 +0400

    fix

--- src/entities/ProfilePreview/ProfilePreview.tsx
+++ src/entities/ProfilePreview/ProfilePreview.tsx
@@ -18,10 +18,11 @@ export const ProfilePreview = observer(() => {
             setImg(image)
         }    
     }, [])
+
     return (
         <div className="profile__preview">
             <div className="profile__avatar">
-                <img src={avatar}/>
+                <img src={img}/>
             </div>
             <div className="profile__info">
                 <h3 className="profile__info-text">
